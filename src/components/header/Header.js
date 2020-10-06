06925eb80a378b867e281b1285986d8dbe4506d4
sunanda <sunandasamanta38@gmail.com>
2020-10-06 14:01:16 +0530

    lodu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,9 +11,9 @@ function Header() {
             <h1 className='header_logo'>Blogger</h1>
             </div>
             <div className='header_right'>
-            <SearchIcon/>
-            <NotificationsOutlinedIcon/>
-            <Avatar alt="Sunanda Samanta" src="https://avatars3.githubusercontent.com/u/46472626?s=460&u=fbdf983ebbbed39b396e12718971d582c123d7f3&v=4"/>
+            <SearchIcon className='header_icons'/>
+            <NotificationsOutlinedIcon className='header_icons'/>
+            <Avatar className='header_icons' alt="Sunanda Samanta" src="https://avatars3.githubusercontent.com/u/46472626?s=460&u=fbdf983ebbbed39b396e12718971d582c123d7f3&v=4"/>
             </div>
         </div>
     )
