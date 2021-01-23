2037a57683c45db7b0d5bc2d1e7f9473017ae3af
sunanda <sunandasamanta38@gmail.com>
2021-01-23 16:51:03 +0530

    final update

--- src/production/firebase.js
+++ src/production/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// we have to use .env file to made this secure
 const firebaseApp = firebase.initializeApp(
   {
     apiKey: "AIzaSyCyuUFCZI7gR4FsL7B46b1JvQ0ZoxAmTnM",
